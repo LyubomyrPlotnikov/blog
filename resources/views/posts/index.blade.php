186134e6b659d4ce44e1a9058cb3fd1f6a3c442e
liubomyr.plotnikov <liubomyr.plotnikov@sigma.software>
2018-03-11 14:38:20 +0200

    Added delete/edit post functionality. Fixed travis configuration.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,8 @@
                     <div class="card mb-4">
                         <div class="card-header">
                             <h2>{{ $post->title }}</h2>
+                            <a href="{{ route('posts.edit', ['id' => $post->id]) }}" class="btn-sm btn-primary">{{ __('Edit') }}</a>
+                            <a href="{{ route('posts.destroy', ['id' => $post->id]) }}" class="btn-sm btn-danger">{{ __('delete') }}</a>
                         </div>
                         <div class="card-body">
                             <p class="card-text">{{ $post->body }}</p>
